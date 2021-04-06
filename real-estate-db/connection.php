8abd7cc0a18d70cfa9ed5d9d9b2296eb9fab0ecd
sydneykwok <47401206+sydneykwok@users.noreply.github.com>
2021-04-06 12:55:52 -0600

    Update connection.php

--- real-estate-db/connection.php
+++ real-estate-db/connection.php
@@ -3,7 +3,7 @@
 // make our connection to the database
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "Iamamysql";  // remove password before sending to other ppl lol !!!!!
+$db_pass = "";  // ENTER YOUR PASSWORD FROM STEP 1 HERE
 $db_name = "real_estate_db";
 
 if (!$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name)) {
