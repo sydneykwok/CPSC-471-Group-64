4413c1e4d08a80eb228ccfed6efa6021c3981a33
sydneykwok <47401206+sydneykwok@users.noreply.github.com>
2021-04-11 20:56:02 -0600

    added bookMeeting backend (writes to online_meeting, tour, online_meeting_has_tour)

--- real-estate-db/bookMeeting.php
+++ real-estate-db/bookMeeting.php
@@ -2,15 +2,53 @@
 // on every page where you want to use the session (check if user is logged in), have to put session_start().
 session_start();
 
-   // include the files we need
-    include("connection.php");	// for use of $conn
-    include("functions.php");	// for use of check_login()
+  // include the files we need
+  include("connection.php");	// for use of $conn
+  include("functions.php");	// for use of check_login()
 
-    // collect user data in user_data variable
-    // check_login() is a function defined in functions.php that takes connection to database in $conn variable
-    $user_data = check_login($conn);	
+  // collect user data in user_data variable
+  // check_login() is a function defined in functions.php that takes connection to database in $conn variable
+  $user_data = check_login($conn);	
 	// if user is not logged in, they will be redirected to login page
 	// if they are logged in, this user_data will contain their info. to access an attribute of the user: $user_data['attribute']
+
+  if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // something was posted, so collect the data from the post variable
+    $agent = $_POST['agent'];
+    $date = $_POST['datepicker'];
+    $time = $_POST['time'] . ":00:00";
+    $tour = $_POST['tour'];
+    $property = $_POST['property'];
+    $message = $_POST['subject'];
+    $b_email = $user_data['Email'];
+
+    if (!empty($date)) {
+      if (!empty($tour)) {
+        // add the meeting to the online_meeting relation
+        $query = "insert into online_meeting (Agent_ID, Date, Time, B_Email, Message) values ('$agent', '$date', '$time', '$b_email', '$message')";
+        mysqli_query($conn, $query);
+        // get the meeting id of the meeting that was just created in the db
+        $meeting_id = mysqli_insert_id($conn);
+
+        if ($tour == "y") {
+          // if the user wants a tour, add it to the tour relation
+          $query = "insert into tour (Property_ID, Agent_ID) values ('$property', '$agent')";
+          mysqli_query($conn, $query);
+          // get the tour id of the tour that was just created in the db
+          $tour_id = mysqli_insert_id($conn);
+          // & add this info to the online_meeting_has_tour relation
+          $query = "insert into online_meeting_has_tour (Tour_ID, Meeting_ID) values ('$tour_id', '$meeting_id')";
+          mysqli_query($conn, $query);  
+        }
+
+        echo "Success!";
+      } else {
+        echo "Please select whether or not you would like a tour.";
+      }
+    } else {
+      echo "Please choose a date.";
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -93,35 +131,51 @@ input[type=submit]:hover {
     </div>
     <div class="column">
       <form method="post">
-        <label for="fname">First Name:</label>
-        <input type="text" id="fname" name="firstname" placeholder="Your name...">
-
-        <label for="lname">Last Name:</label>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name...">
-
-        <label for="ctry">Country:</label>
-        <input type="text" id="ctry" name="country" placeholder="Your country...">
-
-        <label for="addr">Address:</label>
-        <input type="text" id="addr" name="address" placeholder="The address of interested property...">
-
-        <label for="date">Date:</label>
-        <input type="text" id="date" name="address" placeholder="The preferred date of the meeting...">
-
-        <label for="time">Select a Timeslot:</label>
-        <select id="time" name="time">
-          <option value="9-9:45">9:00am-9:45am</option>
-          <option value="10-10:45">10:00am-10:45am</option>
-          <option value="11-11:45">11:00am-11:45am</option>
-          <option value="12-12:45">12:00pm-12:45pm</option>
-          <option value="1-1:45">1:00pm-1:45pm</option>
-          <option value="2-2:45">2:00pm-2:45pm</option>
-          <option value="3-3:45">3:00pm-3:45pm</option>
-          <option value="4-4:45">4:00pm-4:45pm</option>
-          <option value="5-5:45">5:00pm-5:45pm</option>
-          <option value="6-6:45">6:00pm-6:45pm</option>
+        <label for="agentlabel">Agent (ID):</label>
+        <?php 
+          // for each agent in the real_estate_agent table, add their id as an option
+          $query = "select Agent_ID from real_estate_agent";
+          $result = mysqli_query($conn, $query);
+        ?>
+        <select id = "agent" name="agent">
+          <?php while($option = mysqli_fetch_assoc($result)){ ?>
+            <option value="<?php echo $option['Agent_ID']; ?>"><?php echo $option['Agent_ID']; ?></option>
+          <?php } ?>
         </select>
-        
+
+        <label for="datelabel">Date:</label>
+        <input type="date" id="start" name="datepicker" min="2021-04-11" max="2022-04-30"></br></br>
+
+        <label for="timelabel">Time:</label>
+        <select id="time" name="time">
+          <option value="09">9:00am</option>
+          <option value="10">10:00am</option>
+          <option value="11">11:00am</option>
+          <option value="12">12:00pm</option>
+          <option value="13">1:00pm</option>
+          <option value="14">2:00pm</option>
+          <option value="15">3:00pm</option>
+          <option value="16">4:00pm</option>
+          <option value="17">5:00pm</option>
+          <option value="18">6:00pm</option>
+        </select>
+
+        <label for="propertylabel">Property ID:</label>
+        <?php 
+          // for each property in the property table, add its id as an option
+          $query = "select Property_ID from property";
+          $result = mysqli_query($conn, $query);
+        ?>
+        <select id = "property" name="property">
+          <?php while($option = mysqli_fetch_assoc($result)){ ?>
+            <option value="<?php echo $option['Property_ID']; ?>"><?php echo $option['Property_ID']; ?></option>
+          <?php } ?>
+        </select>
+
+        <label>Would you like the meeting to include a tour? </label> 
+				<input id="radbtn" type="radio" name="tour" value="y"/>Yes
+				<input id="radbtn" type="radio" name="tour" value="n"/>No</br></br>
+
         <label for="subject">Subject:</label>
         <textarea id="subject" name="subject" placeholder="Write an additional message..." style="height:170px"></textarea>
         <input type="submit" value="Submit">
